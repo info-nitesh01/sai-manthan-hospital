1eda9e4e086544dc5969a8f2fa72755ca948badb
nitesh <itsnitesh01@gmail.com>
2026-09-20 16:44:06 +0530

    refine preloader

--- partials/preloader.php
+++ partials/preloader.php
@@ -1,7 +1,7 @@
 ﻿<div class="preloader">
     <div class="loading-container">
         <svg class="ecg-loader" viewBox="0 0 220 100" role="img" aria-label="Loading">
-            <path class="ecg-loader-track" d="M0 50H48L58 50L67 10L78 90L90 50H112L121 50L132 25L143 75L154 50H220" />
+            <path class="ecg-loader-track" pathLength="1" d="M0 50H48L58 50L67 10L78 90L90 50H112L121 50L132 25L143 75L154 50H220" />
         </svg>
     </div>
 </div>
